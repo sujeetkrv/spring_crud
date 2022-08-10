f507d3c9a11e2524b499bd8db03eb44faeba6207
suryavanshi <suryavanshi.suryavanshi@leaseplan.com>
2022-08-10 09:24:35 +0200

    first spring crud example commit

---
+++ src/main/java/com/javatechiee/crud/example/repository/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.javatechiee.crud.example.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.javatechiee.crud.example.entity.Product;
+
+public interface ProductRepository extends JpaRepository<Product, Integer>{
+
+	Product findByName(String name);
+
+}
---
+++ src/test/java/com/javatechiee/crud/example/SpringBootCrudExample3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechiee.crud.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCrudExample3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
